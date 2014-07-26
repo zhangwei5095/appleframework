f504ace107a4bd45edc4658fc6cb4d6d8eec4378
xushaomin <xushaomin@foxmail.com>
2014-07-26 14:15:01 +0800

    新增mybatis分页插件 1.采用了拦截器，就是mybaits自带的plus功能。将每次select操作都进行拦截

---
+++ appleframework-persistent-mybatis/src/java/main/com/appleframework/persistent/mybatis/page/Query.java
@@ -0,0 +1,57 @@
+package com.appleframework.persistent.mybatis.page;
+
+import java.util.Map;
+
+import com.appleframework.model.page.Pagination;
+
+/**
+ * 封装查询蚕食和查询条件
+ * 
+ * @author cruise.xu
+ * 
+ */
+public class Query {
+	
+	private Map<String, Object> queryParams;
+	private Pagination page;
+
+	public Map<String, Object> getQueryParams() {
+		return queryParams;
+	}
+
+	public void setQueryParams(Map<String, Object> queryParams) {
+		this.queryParams = queryParams;
+	}
+
+	public Pagination getPage() {
+		return page;
+	}
+
+	public void setPage(Pagination page) {
+		this.page = page;
+	}
+	
+	public Query(){}
+	
+	public Query(Pagination page){
+		this.page = page;
+	}
+	
+	public Query(Pagination page, Map<String, Object> queryParams){
+		this.page = page;
+		this.queryParams = queryParams;
+	}
+	
+	public static Query create() {
+		return new Query();
+	}
+
+	public static Query create(Pagination page, Map<String, Object> queryParams) {
+		return new Query(page, queryParams);
+	}
+	
+	public static Query create(Pagination page) {
+		return new Query(page);
+	}
+
+}
