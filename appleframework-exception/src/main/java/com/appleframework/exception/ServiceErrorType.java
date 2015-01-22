4bf8de8264eb99c2e4db851b48c635b95f01f577
xushaomin <xushaomin@foxmail.com>
2015-01-22 18:16:24 +0800

    修改Exception对象

--- appleframework-exception/src/main/java/com/appleframework/exception/ServiceErrorType.java
+++ appleframework-exception/src/main/java/com/appleframework/exception/ServiceErrorType.java
@@ -12,28 +12,30 @@ import java.util.EnumMap;
  */
 public enum ServiceErrorType {
 	
-	SERVICE_UNAVAILABLE,
-	BUSINESS_LOGIC_ERROR;
+    RSP_SERVICE_UNAVAILABLE,
+    RSP_SERVICE_TIMEOUT,
 
-    private static EnumMap<ServiceErrorType, String> errorCodeMap = new EnumMap<ServiceErrorType, String>(ServiceErrorType.class);
-    private static EnumMap<ServiceErrorType, String> errorMsgMap  = new EnumMap<ServiceErrorType, String>(ServiceErrorType.class);
+    RSV_NOT_EXIST,
+    RSV_INVALID_PERMISSION,
+    RSV_MISSING_PARAMETER,
+    RSV_INVALID_PARAMETE,
+    RSV_PARAMETERS_MISMATCH;
+
+    private static EnumMap<ServiceErrorType, String> errorKeyMap = new EnumMap<ServiceErrorType, String>(ServiceErrorType.class);
 
     static {
-    	
-    	errorCodeMap.put(ServiceErrorType.SERVICE_UNAVAILABLE,  "1");
-    	errorCodeMap.put(ServiceErrorType.BUSINESS_LOGIC_ERROR, "9");
-    	        
-        errorMsgMap.put(ServiceErrorType.SERVICE_UNAVAILABLE,  "服务不可用");
-        errorMsgMap.put(ServiceErrorType.BUSINESS_LOGIC_ERROR, "业务逻辑出错");
-        
+        errorKeyMap.put(ServiceErrorType.RSP_SERVICE_UNAVAILABLE, "rsp.xxx-service-unavailable");
+        errorKeyMap.put(ServiceErrorType.RSP_SERVICE_TIMEOUT, "rsp.xxx-service-timeout");
+
+        errorKeyMap.put(ServiceErrorType.RSV_NOT_EXIST, "rsv.xxx-not-exist:invalid-yyy");
+        errorKeyMap.put(ServiceErrorType.RSV_MISSING_PARAMETER, "rsv.missing-parameter:xxx");
+        errorKeyMap.put(ServiceErrorType.RSV_INVALID_PARAMETE, "rsv.invalid-paramete:xxx");
+        errorKeyMap.put(ServiceErrorType.RSV_INVALID_PERMISSION, "rsv.invalid-permission");
+        errorKeyMap.put(ServiceErrorType.RSV_PARAMETERS_MISMATCH, "rsv.parameters-mismatch:xxx-and-yyy");
     }
-    
-    public static String codeValue(ServiceErrorType serviceErrorType) {
-        return errorCodeMap.get(serviceErrorType);
-    }
-    
-    public static String msgValue(ServiceErrorType serviceErrorType) {
-        return errorMsgMap.get(serviceErrorType);
+
+    public String value() {
+        return errorKeyMap.get(this);
     }
 }
 
--- appleframework-exception/src/main/java/com/appleframework/exception/ServiceException.java
+++ appleframework-exception/src/main/java/com/appleframework/exception/ServiceException.java
@@ -1,42 +1,25 @@
-/*
- * CloudframeworkException.java
- *
- * 深圳广联赛讯有限公司
- *
- * Copyright (C) 2012 WONDERSHARE.COM
- *
- * All Right reserved
- * http://www.glsx.com.cn
- */
 package com.appleframework.exception;
 
-import com.appleframework.config.core.PropertyConfigurer;
-
 /**
- * Base class for all custom exception thrown in CloudFramework
+ * Base class for all custom exception thrown in AppleFramework
  * 
  * @author Cruise.Xu
  * @date: 2012-10-15
- *
+ * 
  */
 public class ServiceException extends Exception {
-	
-	/**
-     * uid used for serialization
-     */
+
 	private static final long serialVersionUID = 7696865849245536841L;
-	
-	/**
-     * Exception code 
-     */
+
+	public static final String RSP = "rsp.";
+
 	private String code;
-	
-	/**
-     * Exception message 
-     */
+
+	private String key;
+
 	private String message;
-	
-    public String getCode() {
+
+	public String getCode() {
 		return code;
 	}
 
@@ -48,67 +31,45 @@ public class ServiceException extends Exception {
 		this.message = message;
 	}
 
-	/**
-     * Default constructor
-     */
-    public ServiceException() {
-        super();
-    }
-
-    /**
-     * Create a new CloudframeworkException from a message which explain the nature of
-     * the Exception
-     */
-    public ServiceException(String code) {
-    	this.code = code;
-    }
-
-    public ServiceException(String code, String message) {
-    	this.code = code;
-        this.message = message;;
-        
-    }
-    /**
-     * Create a new CloudframeworkException from a message and a base throwable
-     * exception
-     */
-    public ServiceException(String message, Throwable throwable) {
-        super(message, throwable);
-    }
-    
-    public ServiceException(String code, String message, Throwable throwable) {
-        super(message, throwable);
-        this.code = code;
-        this.message = message;
-    }
-    
-    public ServiceException(ServiceErrorType serviceErrorType) {
-		super(ServiceErrorType.msgValue(serviceErrorType));
-		this.code = ServiceErrorType.codeValue(serviceErrorType);
-	}
-	
-	public ServiceException(ServiceErrorType serviceErrorType, Throwable throwable) {
-		super(ServiceErrorType.msgValue(serviceErrorType), throwable);
-		this.code = ServiceErrorType.codeValue(serviceErrorType);
-	}
-	
-	public ServiceException(ServiceErrorType serviceErrorType, String message, Throwable throwable) {
-		super(ServiceErrorType.msgValue(serviceErrorType) + ":" + message, throwable);
-		this.code = ServiceErrorType.codeValue(serviceErrorType);
+	public ServiceException() {
+		super();
 	}
 
-	@Override
+	public ServiceException(String code) {
+		this.code = code;
+	}
+
+	public ServiceException(String code, String message) {
+		this.code = code;
+		this.message = message;
+	}
+
+	public ServiceException(String code, String key, String message) {
+		this.code = code;
+		this.key = key;
+		this.message = message;
+	}
+
+	public ServiceException(String message, Throwable throwable) {
+		super(message, throwable);
+	}
+
+	public ServiceException(String code, String message, Throwable throwable) {
+		super(message, throwable);
+		this.code = code;
+		this.message = message;
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
 	public String getMessage() {
-		if(null != super.getMessage()) {
-			return super.getMessage();
-		}
-		if(null != this.message) {
-			return this.message;
-		}
-		if(null != code) 
-			return PropertyConfigurer.getValue(code);
-		return this.message;
+		return message;
 	}
-	
-}
 
+}
